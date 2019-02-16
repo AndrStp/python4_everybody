141398cb0923b072ce93cd14661a6b2786ad77f0
Andriy Stepanenko <47579173+AndrStp@users.noreply.github.com>
2019-02-16 14:33:14 +0200

    Update exercise_04_07.py

--- exercise_04_07.py
+++ exercise_04_07.py
@@ -2,11 +2,13 @@
 #'Python for everybody: Exploring Data Using Python 3' by Charles R. Severance.
 
 x=input('Enter score: ')
+
 try:
     score=float(x)
 except:
     print('Bad score')
     quit()
+	
 def computegrade (score):
     if score > 0 and score < 1:
 	    if score >= .9:
